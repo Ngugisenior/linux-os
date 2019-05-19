094c3cd26ba42794974e42de0fecd6c07a61d1f5
Joseph Ngugi Muiruri <ngugijoseph992@gmail.com>
2019-05-19 23:38:58 +0200

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
 echo "Hello learner"
+#Your task is to use for loops to display only odd natural numbers from  to .
+for number in {1..99..2}
+do
+    echo $number
+done
+############################################################################################################################################
+read name
+echo "Welcome $name"
+#########################################################################################################################################
+for number in {1..50}
+do
+    echo $number
+done
+##############################################################################################################################################################
+read firstNumber
+read lastNumber
+
+echo $((firstNumber+lastNumber))
+echo $((firstNumber - lastNumber))
+echo $((firstNumber * lastNumber))
+echo $((firstNumber / lastNumber))
+############################################################################################################################################
+read firstNumber
+read lastNumber
+
+if(($firstNumber<$lastNumber));then
+    echo X is less than Y;
+elif(($firstNumber>$lastNumber));then
+    echo X is greater than Y;
+else
+    echo X is equal to Y;
+fi
+################################################################################################################################################################
+
+read input
+
+if [ $input = "Y" ] || [ $input = "y" ]; then
+echo YES
+else
+echo NO
+fi
